1009ecc8693714dfc2fb361ad047ebbea201cca8
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2024-10-13 00:41:49 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,24 +1,24 @@
 sfdx force:org:create -f config/project-scratch-def.json -d 1 -s -a sustainabilityref2
 # https://login.salesforce.com/packaging/installPackage.apexp?p0=04t3k000001YOKKAA4
-sfdx force:package:install --package 04t3k0000027CZ1AAM -w 20
+sf package install --package 04t3k0000027CZ1AAM -w 20
 
 
 # Special perm/license stuff for SustCloud
 sfdx shane:user:psl -l User -g User -n sustain_app_SustainabilityCloudPsl
-sfdx force:user:permset:assign -n SustainabilityAnalytics
-sfdx force:user:permset:assign -n SustainabilityAppAuditor
-sfdx force:user:permset:assign -n SustainabilityAppManager
-sfdx force:user:permset:assign -n SustainabilityCloud
+sf org assign permset -n SustainabilityAnalytics
+sf org assign permset -n SustainabilityAppAuditor
+sf org assign permset -n SustainabilityAppManager
+sf org assign permset -n SustainabilityCloud
 sfdx shane:user:psl -l User -g Integration -n InsightsBuilderPsl
-sfdx shane:user:permset:assign -l User -g Integration -n EinsteinAnalyticsUser
-sfdx shane:user:permset:assign -l User -g Integration -n SustainabilityAnalytics
+sf org assign permset -l User -g Integration -n EinsteinAnalyticsUser
+sf org assign permset -l User -g Integration -n SustainabilityAnalytics
 
-sfdx automig:load -d automig --concise --mappingobjects RecordType:DeveloperName
+sf automig load -d automig --concise --mappingobjects RecordType:DeveloperName
 #sfdx automig:load -d data --concise --mappingobjects RecordType:DeveloperName
 
-sfdx analytics:app:create -f assets/analyticsAppCreateDef.json -w 60
+sf analytics app create -f assets/analyticsAppCreateDef.json -w 60
 sfdx shane:analytics:app:share -n Sustainability --org
-sfdx force:source:push
-sfdx force:user:permset:assign -n SustainabilityCustom
+sf project deploy start
+sf org assign permset -n SustainabilityCustom
 
-sfdx force:org:open -p /lightning/app/c__Sustainability_Console
+sf org open -p /lightning/app/c__Sustainability_Console
